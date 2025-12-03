f7efef5cd800f70eb45379b43a5d3e4e790afbed
fredhkris <frede.h.k1@gmail.com>
2025-12-03 22:25:16 +0100

    Revert "Rough implementation of Event related classes"
    This reverts commit 91e6801c2a47c410518f83ae988b50df90c52d80.

--- ProjectClassLibrary/Models/Event.cs
+++ ProjectClassLibrary/Models/Event.cs
@@ -1,5 +1,4 @@
-﻿using ProjectClassLibrary.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,28 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProjectClassLibrary.Models
 {
-    public class Event : IEvent
+    public class Event 
     {
-        private static int _counter;
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-
-        public Event(string name, string description, DateTime startDate, DateTime endDate)
-        {
-            Name = name;
-            Description = description;
-            StartDate = startDate;
-            EndDate = endDate;
-            Id = _counter++;
-        }
-
-        public override string ToString()
-        {
-            return "";
-        }
     }
 }
