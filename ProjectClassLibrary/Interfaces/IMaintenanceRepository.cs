2ee3cc3336e164ca648542912e36b63110eea00d
Neozt001 <neo001@edu.zealand.dk>
2025-12-04 09:31:09 +0100

    IMaintenance, IMaintenanceRepository, Maintenance, MaintenanceRepository

---
+++ ProjectClassLibrary/Interfaces/IMaintenanceRepository.cs
@@ -0,0 +1,25 @@
+﻿using ProjectClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//--Lavet af Necati--
+namespace ProjectClassLibrary.Interfaces
+{
+    public interface IMaintenanceRepository
+    {
+         
+        void AddMaintenance(Maintenance maintenance);
+        void RemoveMaintenance(int id);
+        void PrintAll();
+        Maintenance GetMaintenanceById(int id);
+        List<Maintenance> GetPendingMaintenanceTasks();
+        List<Maintenance> GetCompletedMaintenanceTasks();
+        List<Maintenance> GetAll();
+        
+        
+    }
+}
