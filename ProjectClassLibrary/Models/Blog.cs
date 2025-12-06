c2ba118bfdb22389ca3a8c3e90bae0921cbc15b2
fredhkris <frede.h.k1@gmail.com>
2025-12-06 18:53:21 +0100

    Add regions to Blog

--- ProjectClassLibrary/Models/Blog.cs
+++ ProjectClassLibrary/Models/Blog.cs
@@ -9,15 +9,20 @@ namespace ProjectClassLibrary.Models
 {
     public class Blog : IBlog
     {
+        #region Fields
         private static int _counter;
+        #endregion
 
+        #region Properties
         public int Id { get; set; }
         public string Headline { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
         public string Author { get; set; }
         public string Picture { get; set; }
+        #endregion
 
+        #region Constructors
         public Blog(string headline, string description, DateTime date, string author, string picture)
         {
             Headline = headline;
@@ -27,10 +32,13 @@ namespace ProjectClassLibrary.Models
             Picture = picture;
             Id = _counter++;
         }
+        #endregion
 
+        #region Methods
         public override string ToString()
         {
             return $"Id: {Id}, \nHeadline: {Headline}, \nDescription: {Description}, \nDate: {Date}, \nAuthor: {Author}, \nPicture: {Picture}";
         }
+        #endregion
     }
 }
