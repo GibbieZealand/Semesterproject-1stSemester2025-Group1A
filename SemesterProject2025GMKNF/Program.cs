622cf217586c9063b3f3347902cad9fc7c6c0abc
KasperKronmannMoeller <kam002@edu.zealand.dk>
2025-12-08 11:10:15 +0100

    Oprettet region i main program.cs

--- SemesterProject2025GMKNF/Program.cs
+++ SemesterProject2025GMKNF/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+#region Blog og BlogRepo
+
+#endregion
+#region Boat og BoatRepo
+
+#endregion
+#region Booking og BookingRepo
+
+#endregion
+#region Event og EventRepo
+
+#endregion
+#region Maintenance og MaintenanceRepo
+
+#endregion
+#region Member og MemberRepo
+
+#endregion
+
+
