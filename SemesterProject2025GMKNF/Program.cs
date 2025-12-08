a14cb0c7d0203d7fba11bf3ec7b20d94be9e8831
KasperKronmannMoeller <kam002@edu.zealand.dk>
2025-12-08 12:11:59 +0100

    Regions i Main program.cs slettet

--- SemesterProject2025GMKNF/Program.cs
+++ SemesterProject2025GMKNF/Program.cs
@@ -1,22 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-#region Blog og BlogRepo
 
-#endregion
-#region Boat og BoatRepo
-
-#endregion
-#region Booking og BookingRepo
-
-#endregion
-#region Event og EventRepo
-
-#endregion
-#region Maintenance og MaintenanceRepo
-
-#endregion
-#region Member og MemberRepo
-
-#endregion
 
 
