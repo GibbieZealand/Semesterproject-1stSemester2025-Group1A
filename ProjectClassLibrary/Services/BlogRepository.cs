fe3f935b07e4c14268576203afd4b9afde754378
fredhkris <frede.h.k1@gmail.com>
2025-12-12 10:14:25 +0100

    Add crude documentation to BlogRepository

--- ProjectClassLibrary/Services/BlogRepository.cs
+++ ProjectClassLibrary/Services/BlogRepository.cs
@@ -22,6 +22,10 @@ namespace ProjectClassLibrary.Services
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Adds a new blog to the blog list, 
+        /// if a previous blog with the same id doesn't already exist
+        /// </summary>
         public void AddBlog(IBlog blog)
         {
             foreach (IBlog b in _blogs)
@@ -33,30 +37,41 @@ namespace ProjectClassLibrary.Services
             }
             _blogs.Add(blog);
         }
-
+        /// <summary>
+        /// Removes a blog from the blog list
+        /// </summary>
         public void RemoveBlog(IBlog blog)
         {
             _blogs.Remove(blog);
         }
-
+        /// <summary>
+        /// "Updates" a blog in the blog list,
+        /// by replacing it with a new blog, but keeping the old id
+        /// </summary>
         public void UpdateBlog(int id, IBlog newBlog)
         {
             for (int i = 0; i < _blogs.Count; i++)
             {
-                if (_blogs[i].Id == id)
+                int existingId = _blogs[i].Id;
+                if (existingId == id)
                 {
-                    newBlog.Id = _blogs[i].Id;
+                    newBlog.Id = existingId;
                     _blogs[i] = newBlog;
                     return;
                 }
             }
         }
 
+        /// <summary>
+        /// Returns our blog list
+        /// </summary>
         public List<IBlog> GetAllBlogs()
         {
             return _blogs;
         }
-
+        /// <summary>
+        /// Prints all blogs in the blog list
+        /// </summary>
         public void PrintAll()
         {
             foreach (IBlog b in _blogs)
