ded7ecc2b0687683df6e7e899206ccac5e3221b4
Maria Litske Strauss <miiaam@live.dk>
2025-12-15 11:06:05 +0100

    Added regions and comments to NecatiTest to increase readability

--- NecatiTest/Program.cs
+++ NecatiTest/Program.cs
@@ -3,21 +3,31 @@ using ProjectClassLibrary.Interfaces;
 using ProjectClassLibrary.Models;
 using ProjectClassLibrary.Services;
 
-// Tests of Maintenance class and repository
+#region Nye objekter af boat, member og maintenance skabes
 IBoat jolle1 = new Boat(BoatType.TERA, "Model", "16-3335", "Is very good :3", 32, 23, 33, "1982");
 IMember member1 = new Member("Kasper", "Møller", "23456789", "Skovvej 111", "Næstved", "Kasper@hotmail.com", MemberType.Adult, MemberRole.Member);
 
 Maintenance m1 = new Maintenance("Hul i skroget", DateTime.Now,jolle1,member1);
 Maintenance m2 = new Maintenance("Hul i dækket", DateTime.Now,jolle1,member1);
+#endregion
 
+#region Test af AddMaintenance
+Console.WriteLine("Test af AddMaintenance metode...");
 MaintenanceRepository mRepo = new MaintenanceRepository();
 mRepo.AddMaintenance(m1);
 mRepo.AddMaintenance(m2);
-
-m2.Fixed();
-
 mRepo.PrintAll();
+#endregion
 
+#region Test af Fixed metode
+Console.WriteLine("Test af Fixed metode...");
+m2.Fixed();
+Console.WriteLine(m2);
+#endregion
+
+#region Test af exception
+Console.WriteLine("");
+Console.WriteLine("Test af exception...");
 try
 {
     m2.Fixed();
@@ -26,3 +36,4 @@ catch (BoatAlreadyFixedException ex)
 {
     Console.WriteLine(ex.Message);
 }
+#endregion
