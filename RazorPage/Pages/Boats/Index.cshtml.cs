56b2db81f768d3e4913b6f1c883c1d0955ee3a4b
Maria Litske Strauss <miiaam@live.dk>
2025-12-15 12:33:07 +0100

    Added Index.cshtml to all folders in RazorPage

---
+++ RazorPage/Pages/Boats/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage.Pages.Boats
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
