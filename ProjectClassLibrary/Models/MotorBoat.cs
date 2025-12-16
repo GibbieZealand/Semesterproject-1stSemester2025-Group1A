c1d8618efa71d37d0eb2a44a514185f805c5da2c
Maria Litske Strauss <miiaam@live.dk>
2025-12-16 10:22:58 +0100

    Added unused SailBoatType enum

--- ProjectClassLibrary/Models/MotorBoat.cs
+++ ProjectClassLibrary/Models/MotorBoat.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// -- ikke i brug - kun til inheritance eksempel
+
 namespace ProjectClassLibrary.Models
 {
     /// <summary>
